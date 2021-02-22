5f1d98656b5540c8d8af2adfcbd52d813075f1fa
arcav <armandojcastro84@gmail.com>
2021-02-22 17:07:39 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '21100d160e51fddc14606cedba9ef409'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8568c9a1-0134-4123-8c7e-67cf53a4e4d8'
+};
